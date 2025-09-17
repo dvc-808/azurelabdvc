47885f7fa7e595e207ffb70e90dff0d1fea36b29
dvcchamhocvcl <dvcchamhoc@gmail.com>
2025-09-17 13:09:12 +0700

    hello

--- app/settings.py
+++ app/settings.py
@@ -5,11 +5,11 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Settings:
     # Key Vault
-    key_vault_url: str = "kv.privatelink.vaultcore.azure.net"
+    key_vault_url: str = "https://cuong-keyvault.vault.azure.net/"
     sql_connection_secret_name: str = "cuongdbstring"
 
     # Storage
-    storage_account_url: str = "sa.privatelink.blob.core.windows.net"
+    storage_account_url: str = "https://dvcdangcapvippro.blob.core.windows.net"
     storage_container_name: str = "profile-image"
 
 
